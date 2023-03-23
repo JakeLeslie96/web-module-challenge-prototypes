9ed7f43ce611cbebed9ec8e4b01256ad3eabd8a2
JakeLeslie96 <jakeleslie96@gmail.com>
2023-03-22 23:16:39 -0400

    Finished Task 1-3, final commit

--- index.js
+++ index.js
@@ -15,9 +15,25 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function (edible){
+  if (this.stomach.length < 10){
+    this.stomach.push(edible)
+  }
+}
+
+Person.prototype.poop = function (){
+  this.stomach = []
+}
+Person.prototype.toString = function (){
+  return `${this.name}, ${this.age}`
+}
+
+
 
 
 /*
@@ -36,8 +52,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
+}
+Car.prototype.fill = function (gallons){
+  this.tank = this.tank + gallons
 }
 
 
@@ -49,10 +71,16 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age, favoriteToy)
+  this.favoriteToy = favoriteToy
 }
 
+Baby.prototype = Object.create(Person.prototype)
+
+Baby.prototype.play = function (){
+  return `playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
